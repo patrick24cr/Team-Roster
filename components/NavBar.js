767568d360eaebdf9c546f5c32bebce9e692b736
Patrick Burns <burns212@gmail.com>
2022-07-23 12:17:51 -0500

    basic landing page and react refamiliarization

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,20 +8,26 @@ import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navBarContainer0">
+      <Container className="navBarContainer1">
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Band-Aid</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
-          </Nav>
+          <Container className="navBarContainer2">
+            <Nav className="me-auto navBarContainer3">
+              {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
+              <Link passHref href="/">
+                <Nav.Link>My Bands</Nav.Link>
+              </Link>
+              <Link passHref href="/">
+                <Nav.Link>New Band</Nav.Link>
+              </Link>
+              <div className="navBarSpacer" />
+              <Button variant="outline-light" onClick={signOut}>Sign Out</Button>
+            </Nav>
+          </Container>
         </Navbar.Collapse>
       </Container>
     </Navbar>
